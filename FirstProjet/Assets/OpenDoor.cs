aa64d7f887a24676a4d650afd9e4265620153d97
Luca Verardo <luca@verardo.ch>
2021-02-25 22:15:47 +0100

    Added : - Door - Glass - Tree - Ashtray - Particle System - Trigger on door to open/close - EmptyGameObject for door opening / closing - Spotlight - Mini first person controller - Shadows - Antialiasing - Smoke when clicking ashtray - Ground collider for first person controller

---
+++ FirstProjet/Assets/OpenDoor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class OpenDoor : MonoBehaviour
+{
+
+	public GameObject door;
+	public GameObject doorClosed;
+	public GameObject doorOpened;
+
+	private bool opened = false;
+
+    private void OnTriggerEnter()
+    {
+        changeDoorState();
+    }
+
+    private void OnTriggerExit()
+    {
+        changeDoorState();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void changeDoorState()
+    {
+        if (opened)
+        {
+            // Close the door
+            door.transform.DOMove(doorClosed.transform.position, 1f);
+            door.transform.DORotateQuaternion(doorClosed.transform.rotation, 1f);
+        }
+        else
+        {
+            // Open the door
+            door.transform.DOMove(doorOpened.transform.position, 1f);
+            door.transform.DORotateQuaternion(doorOpened.transform.rotation, 1f);
+        }
+
+        opened = !opened;
+    }
+	
+}
---
+++ FirstProjet/Assets/SmokeSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeSpawner : MonoBehaviour
+{
+    public ParticleSystem smokeEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        smokeEffect.Stop();
+    }
+
+    private void OnMouseDown()
+    {
+        if (smokeEffect.isPlaying)
+        {
+            smokeEffect.Stop();
+        }
+        else
+        {
+            smokeEffect.Play();
+        }
+    }
+}
