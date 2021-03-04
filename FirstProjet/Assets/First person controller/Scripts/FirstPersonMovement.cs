29220486e5b1460e324e2d36b8e502825e08d32d
Luca Verardo <luca@verardo.ch>
2021-03-04 22:30:01 +0100

    Added Blend Tree

--- FirstProjet/Assets/First person controller/Scripts/FirstPersonMovement.cs
+++ FirstProjet/Assets/First person controller/Scripts/FirstPersonMovement.cs
@@ -14,6 +14,13 @@ public class FirstPersonMovement : MonoBehaviour
     /// <summary> Functions to override movement speed. Will use the last added override. </summary>
     public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
 
+    public GameObject cyborgObject;
+    Vector3 previousPosition;
+
+    private void Start()
+    {
+        previousPosition = cyborgObject.transform.position;
+    }
 
     void FixedUpdate()
     {
@@ -21,9 +28,22 @@ public class FirstPersonMovement : MonoBehaviour
         IsRunning = canRun && Input.GetKey(runningKey);
         float movingSpeed = IsRunning ? runSpeed : speed;
         if (speedOverrides.Count > 0)
+        {
             movingSpeed = speedOverrides[speedOverrides.Count - 1]();
+        }
+
         velocity.y = Input.GetAxis("Vertical") * movingSpeed * Time.deltaTime;
         velocity.x = Input.GetAxis("Horizontal") * movingSpeed * Time.deltaTime;
+
+        float playerVelocity = ((cyborgObject.transform.position - previousPosition).magnitude) / Time.deltaTime;
+        previousPosition = cyborgObject.transform.position;
+
+        Animator cyborgAnimator = cyborgObject.GetComponent<Animator>();
+        cyborgAnimator.SetBool("isMoving", playerVelocity > 0);
+
+        cyborgAnimator.SetFloat("Speed", Input.GetAxis("Vertical") * movingSpeed);
+        cyborgAnimator.SetFloat("Direction", Input.GetAxis("Horizontal") * movingSpeed);
+
         transform.Translate(velocity.x, 0, velocity.y);
     }
 }
